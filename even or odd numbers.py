16e7d6a3d20b9bc1a4b574f7def8dd58170e758b
unregisteredRam <aldreesm2007@gmail.com>
2025-07-04 19:49:06 +0300

    Delete even or odd numbers.py

--- even or odd numbers.py
+++
@@ -1,7 +0,0 @@
-#even or odd number
-
-num=int(input('Enter your number: '))
-if (num % 2) == 0:
-    print("{0} is an Even number".format(num))
-else:
-    print("{0} is an Odd number".format(num))
