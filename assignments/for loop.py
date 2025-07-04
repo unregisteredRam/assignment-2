028111a29c491470eb8611e811bfd7b8adb28c01
unregisteredRam <aldreesm2007@gmail.com>
2025-07-04 19:51:25 +0300

    Add files via upload

--- assignments/for loop.py
+++ assignments/for loop.py
@@ -1,4 +1,3 @@
-sum=0
 for i in range(1,51):
-    sum=sum+ i
-print(f'the sum of numbers from 1 to 50 is: {sum}')
+    sum(range(1,51))
+print(f'the sum of numbers from 1 to 50 is: {sum(range(1,51))}')
